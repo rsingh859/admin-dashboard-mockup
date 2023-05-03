65586153c7485770050292e23a19a3e0cf70c7d4
RiteshSingh <singh.ritesh2806@gmail.com>
2023-05-03 15:00:42 +0530

    Adding the nivo geographical chart

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Dashboard from "screens/dashboard";
 import Products from "screens/products";
 import Customers from "screens/customers";
 import Transactions from "screens/transactions";
+import Geography from "screens/geography";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -26,6 +27,7 @@ function App() {
               <Route path="/products" element={<Products />} />
               <Route path="/customers" element={<Customers />} />
               <Route path="/transactions" element={<Transactions />} />
+              <Route path="/geography" element={<Geography />} />
             </Route>
           </Routes>
         </ThemeProvider>
